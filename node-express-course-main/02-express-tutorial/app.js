94bec62d572686f6e9f263a6678db101c615fa4c
disha odedara <dishaodedara@gmail.com>
2024-08-27 14:40:11 +0530

    first commit

---
+++ node-express-course-main/02-express-tutorial/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app=express()
+
+//setup static and middleware
+app.use(express.static('./public'))
+// app.get('/',(req,res)=>{
+//   res.sendFile( path.resolve(__dirname,'./navbar-app/index.html'))
+
+// })
+
+app.listen(5000,()=>{
+  console.log('server is listing at 5000 ')
+   
+
+})
